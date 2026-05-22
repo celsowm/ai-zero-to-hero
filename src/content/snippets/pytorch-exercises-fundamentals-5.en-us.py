2c2ff15187c71a1be6981f863aa0f69d4675d420
celsowm <celsowm@gmail.com>
2026-05-22 00:49:14 -0300

    Update PyTorch fundamentals exercise 1 EN

--- src/content/snippets/pytorch-exercises-fundamentals-5.en-us.py
+++ src/content/snippets/pytorch-exercises-fundamentals-5.en-us.py
@@ -1,7 +1,9 @@
 import torch
 
-# Provided data
+# Provided data: token IDs.
 data = [10, 20, 30]
 
-# TODO: Convert data to a long tensor called 'ids'
+# TODO: Convert data to a long tensor called 'ids'.
+# Then save str(ids.dtype) in 'ids_dtype'.
 ids = None
+ids_dtype = None
