86149de3a75e0e6a354f9b08fda96843b0aad57a
celsowm <celsowm@gmail.com>
2026-05-22 00:51:46 -0300

    Add PyTorch fundamentals exercise 6 PT snippet

---
+++ src/content/snippets/pytorch-exercises-fundamentals-10.pt-br.py
@@ -0,0 +1,8 @@
+import torch
+
+# Ponto de avaliação para autograd.
+x = torch.tensor(3.0, requires_grad=True)
+
+# TODO: Calcule y = x**2, rode backward e salve x.grad.item() em grad_val.
+y = None
+grad_val = None
